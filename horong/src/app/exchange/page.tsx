fa18b4286e48fa1ec02a90a0d29b2c2ca1208e24
최홍석 <che0618@naver.com>
2024-10-25 15:55:42 +0900

    feat: 환전소 페이지 초기 설정

---
+++ horong/src/app/exchange/page.tsx
@@ -0,0 +1,10 @@
+import GoogleMap from '@/features/exchange/components/GoogleMap/index.tsx'
+
+export default function ExchangePage() {
+  return (
+    <div>
+      <p>This is Exchange Page</p>
+      <GoogleMap />
+    </div>
+  )
+}
---
+++ horong/src/features/exchange/components/GoogleMap/index.tsx
@@ -0,0 +1,7 @@
+export default function GoogleMap() {
+  return (
+    <div>
+      <p>This is Google Map compo</p>
+    </div>
+  )
+}
