f0ae7f0d56b71be9e09e2e4b4b6e1f77b6a24b70
최홍석 <che0618@naver.com>
2024-10-25 16:14:56 +0900

    feat: 구글 맵 API 연결 및 컴포넌트 설정

--- horong/src/app/exchange/page.tsx
+++ horong/src/app/exchange/page.tsx
@@ -2,9 +2,11 @@ import GoogleMap from '@/features/exchange/components/GoogleMap/index.tsx'
 
 export default function ExchangePage() {
   return (
-    <div>
+    <div className="w-[22.5rem]">
       <p>This is Exchange Page</p>
-      <GoogleMap />
+      <div className="h-[45rem]">
+        <GoogleMap />
+      </div>
     </div>
   )
 }
--- horong/src/features/exchange/components/GoogleMap/index.tsx
+++ horong/src/features/exchange/components/GoogleMap/index.tsx
@@ -1,7 +1,40 @@
+'use client'
+
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+// import { useState } from 'react'
+
 export default function GoogleMap() {
+  // const [zoom, setZoom] = useState<number>(0)
   return (
-    <div>
-      <p>This is Google Map compo</p>
-    </div>
+    <APIProvider
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string}
+      region="KR"
+    >
+      <Map
+        colorScheme={'DARK'}
+        mapId={'baae3adccbf5d8d'}
+        defaultZoom={15}
+        disableDefaultUI={true}
+        defaultCenter={{ lat: 37.558005440695396, lng: 127.00869391175185 }}
+        onCameraChanged={(event) => {
+          // setZoom(event.detail.zoom)
+          console.log(
+            'camera changed:',
+            event.detail.center,
+            'zoom:',
+            event.detail.zoom,
+          )
+        }}
+      >
+        {/* {zoom >= 13 &&
+          data.map((item) => (
+            <MapMarker
+              key={item.id}
+              latitude={item.latitude}
+              longitude={item.longitude}
+            />
+          ))} */}
+      </Map>
+    </APIProvider>
   )
 }
