124dea27122c38634c5eda1baf7cce79705425f3
coolfin <sorlti6952@gmail.com>
2024-11-05 15:56:56 +0900

    fix: modal에 따른 라우팅 분기 변경

--- horong/src/providers/TanstackQueryProvider/index.tsx
+++ horong/src/providers/TanstackQueryProvider/index.tsx
@@ -47,6 +47,11 @@ export default function TanstackQueryProvider({
   const checkToken = () => {
     const token = sessionStorage.getItem('token')
 
+    if (localStorage.getItem('token') !== null && token === null) {
+      sessionStorage.setItem('token', localStorage.getItem('token') as string)
+      router.push('/home')
+    }
+
     if (token !== null && pathname === '/') {
       router.push('/home')
     } else if (
