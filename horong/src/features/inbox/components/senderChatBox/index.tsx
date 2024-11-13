4c86be8efa3cd40ed95b9f0c44665a0a8e46e725
최홍석 <che0618@naver.com>
2024-11-13 17:19:24 +0900

    feat: 쪽지에서 사진도 보이도록 수정

--- horong/src/features/inbox/components/senderChatBox/index.tsx
+++ horong/src/features/inbox/components/senderChatBox/index.tsx
@@ -28,7 +28,16 @@ function SenderChatBox({ data }: SenderChatBoxProps) {
           <div className="max-w-[17.25rem] rounded-bl-xl rounded-br-xl rounded-tr-xl bg-gradient-to-br from-[#22DFEB] to-[#ACBEFF] p-[.0625rem]">
             <p className="flex rounded-bl-xl rounded-br-xl rounded-tr-xl bg-[#1B1D24] px-2.5 py-1">
               <span className="w-fit hyphens-auto break-all bg-[#1B1D24] text-xs text-white">
-                {data.content}
+                {data.content ? (
+                  data.content
+                ) : (
+                  <Image
+                    src={data.image}
+                    alt={'chat image'}
+                    width={280}
+                    height={228}
+                  />
+                )}
               </span>
             </p>
           </div>
