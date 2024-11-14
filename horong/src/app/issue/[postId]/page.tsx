49da8bb9c925357ce8609f7c017a33cbb06f9b64
coolfin <sorlti6952@gmail.com>
2024-11-14 12:54:16 +0900

    feat: 숏폼 API 연결 및 구현

--- horong/src/app/issue/[postId]/page.tsx
+++ horong/src/app/issue/[postId]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import DOMPurify from 'dompurify'
 import Image from 'next/image'
 import React, { useRef, useState } from 'react'
 import { LoaderIcon } from 'react-hot-toast'
@@ -14,33 +15,87 @@ import ScrapOnSVG from '@/static/svg/issue/issue-post-scrap-on-icon.svg'
 import UnLikeOffSVG from '@/static/svg/issue/issue-post-unlike-off-icon.svg'
 import UnLikeOnSVG from '@/static/svg/issue/issue-post-unlike-on-icon.svg'
 
-interface ShortFormGridType {
-  id: number
-  content: string
-  image: string
-  audio: string
-  is_saved: boolean
-  preference: number
-}
-
 function IssueDetail({ params }: { params: { postId: string } }) {
-  const [liked, setLiked] = useState(false)
-  const [unliked, setUnliked] = useState(false)
   const [scraped, setScraped] = useState(false)
+
+  const [action, setAction] = useState<number>(0) //1: like, 2: unlike 0: none
   const scrollPos = useRef<HTMLDivElement>(null)
 
   const [isCollapsed, setIsCollapsed] = useState(false)
 
   const lang = useLangStore((state) => state.lang)
-
+  const santinizer = DOMPurify.sanitize
   const { data: shorformGrid, isLoading } = useQuery({
     queryKey: ['short-form-grid-detail', params.postId],
     queryFn: async () => {
       const res = await privateAPI.get('/shortForm/' + params.postId)
+
+      if (res.status === 200) {
+        //오디오 플레이
+        const audio = new Audio(res.data.result.audio)
+        audio.play()
+      }
+
       return res.data.result
     },
   })
 
+  const queryClient = useQueryClient()
+  const { mutate: mutateScrap } = useMutation({
+    mutationFn: async () => {
+      const res = await privateAPI.post('/shortForm/is_saved', {
+        shortFormId: params.postId,
+        isSaved: !scraped,
+      })
+      return res.data.result
+    },
+    onSuccess: () => {
+      setScraped(!scraped)
+      queryClient.invalidateQueries({
+        queryKey: ['short-form-grid-detail', params.postId],
+      })
+    },
+  })
+
+  const handleScrap = () => {
+    mutateScrap()
+  }
+
+  const { mutate: mutateLike } = useMutation({
+    mutationFn: async ({ num }: { num: number }) => {
+      const res = await privateAPI.post('/shortForm/preference', {
+        shortFormId: params.postId,
+        preference: num,
+      })
+      return res.data.result
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['short-form-grid-detail', params.postId],
+      })
+    },
+  })
+
+  const handleLike = () => {
+    if (action === 1) {
+      setAction(0)
+      mutateLike({ num: 0 })
+    } else {
+      setAction(1)
+      mutateLike({ num: 1 })
+    }
+  }
+
+  const handleUnLike = () => {
+    if (action === 2) {
+      setAction(0)
+      mutateLike({ num: 0 })
+    } else {
+      setAction(2)
+      mutateLike({ num: 2 })
+    }
+  }
   if (isLoading) {
     return (
       <div className="flex w-full grow items-center justify-center py-10">
@@ -85,10 +140,10 @@ function IssueDetail({ params }: { params: { postId: string } }) {
         {/* icons */}
         <div className="flex flex-col gap-y-5 px-4 py-5">
           <button
-            onClick={() => setLiked(!liked)}
+            onClick={handleLike}
             className="flex flex-col items-center justify-center gap-y-1"
           >
-            {liked ? (
+            {action === 1 ? (
               <LikeOnSVG className="h-6 w-6" />
             ) : (
               <LikeOffSVG className="h-6 w-6" />
@@ -99,10 +154,10 @@ function IssueDetail({ params }: { params: { postId: string } }) {
           </button>
 
           <button
-            onClick={() => setUnliked(!unliked)}
+            onClick={handleUnLike}
             className="flex flex-col items-center justify-center gap-y-1"
           >
-            {unliked ? (
+            {action === 2 ? (
               <UnLikeOnSVG className="h-6 w-6" />
             ) : (
               <UnLikeOffSVG className="h-6 w-6" />
@@ -113,15 +168,18 @@ function IssueDetail({ params }: { params: { postId: string } }) {
           </button>
 
           <button
-            onClick={() => setScraped(!scraped)}
+            onClick={handleScrap}
             className="flex flex-col items-center justify-center gap-y-1"
           >
             <div className="flex h-6 w-6 items-center justify-center">
               {scraped ? <ScrapOnSVG /> : <ScrapOffSVG />}
             </div>
-            <span className="text-2xs">
-              {ISSUE_CONSTANTS[lang]['detail-scrap-text']}
-            </span>
+            <span
+              className="text-2xs"
+              dangerouslySetInnerHTML={{
+                __html: santinizer(ISSUE_CONSTANTS[lang]['detail-scrap-text']),
+              }}
+            />
           </button>
         </div>
       </div>
