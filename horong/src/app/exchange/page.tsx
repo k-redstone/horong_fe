05cf4c89a9d2ae444e3faf2ed605ac43b5e4f6b2
k-redstone <che0618@naver.com>
2024-11-16 01:51:44 +0900

    feat: exchange페이지의 필드 파일을 325kb -> 101kb로 줄임

--- horong/src/app/exchange/page.tsx
+++ horong/src/app/exchange/page.tsx
@@ -1,8 +1,14 @@
 'use client'
+import dynamic from 'next/dynamic'
+
 import GlobalHeader from '@/components/globalHeader/index.tsx'
 import { EXCHANGE_CONSTANT } from '@/constants/exchange/index.ts'
-import GoogleMap from '@/features/exchange/components/GoogleMap/index.tsx'
+// import GoogleMap from '@/features/exchange/components/GoogleMap/index.tsx'
 import useLangStore from '@/hooks/useLangStore.ts'
+const GoogleMap = dynamic(
+  () => import('@/features/exchange/components/GoogleMap/index.tsx'),
+  { ssr: false },
+)
 
 export default function ExchangePage() {
   const lang = useLangStore((state) => state.lang)
