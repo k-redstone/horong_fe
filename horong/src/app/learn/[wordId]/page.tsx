de40c437ea41ca855a22b0fb1fda7b8fe0b58cba
coolfin <sorlti6952@gmail.com>
2024-11-18 13:06:45 +0900

    fix: 학습 기록 컴포넌트 변경

--- horong/src/app/learn/[wordId]/page.tsx
+++ horong/src/app/learn/[wordId]/page.tsx
@@ -4,7 +4,15 @@ import { useQuery } from '@tanstack/react-query'
 
 import privateAPI from '@/api/privateAPI/index.ts'
 import RecordDetailCollapseBtn from '@/features/learn/components/recordDetail/index.tsx'
-interface RecordType {
+// interface RecordType {
+//   word: string
+//   wordId: number
+//   pronunciation: string
+//   definition: string
+//   educationRecordList: EducationRecordType[]
+// }
+
+interface EducationRecordType {
   id: number
   text: string
   cer: number
@@ -26,14 +34,31 @@ function RecordHistoryDetail({ params }: { params: { wordId: string } }) {
     <div className="flex grow flex-col gap-y-3 overflow-y-scroll px-5 py-8">
       {/* <p className="text-sm font-bold text-text-high">{}</p> */}
 
+      <div className="flex w-full flex-col items-center justify-center gap-y-3 rounded-xl bg-grey-70 p-6 text-text-high">
+        <div className="flex flex-col items-center justify-center gap-y-1">
+          <h3 className="text-xl">
+            {detailRecordList && detailRecordList.word}
+          </h3>
+          <p className="text-2xs">
+            {detailRecordList && detailRecordList.pronunciation}
+          </p>
+        </div>
+        <div className="whitespace-pre-line text-sm">
+          {detailRecordList && detailRecordList.definition}
+        </div>
+      </div>
+
       <div className="flex flex-col gap-y-1 px-1">
         {detailRecordList &&
-          detailRecordList.map((record: RecordType) => (
-            <RecordDetailCollapseBtn
-              key={record.id}
-              record={record}
-            />
-          ))}
+          detailRecordList.educationRecordList.map(
+            (record: EducationRecordType, index: number) => (
+              <RecordDetailCollapseBtn
+                key={record.id}
+                index={index}
+                record={record}
+              />
+            ),
+          )}
       </div>
     </div>
   )
